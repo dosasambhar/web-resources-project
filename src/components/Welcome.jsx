a3981733791a68d2e61f20c46b93484b19cf6ab6
dosasambhar <jhaurvija2002@gmail.com>
2023-08-09 01:56:53 +0530

    changes

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,7 @@
 const Welcome = ({ section }) => {
   return (
-    <div className="mx-auto text-center">
-      <h2 className="text-4xl font-roboto tracking-wider mb-2">
+    <div className="mx-auto  text-center">
+      <h2 className="text-5xl font-roboto tracking-wider mb-2">
         {`Welcome to ${section} section!`}
       </h2>
       <p className="font-roboto tracking-wider">Select a Tag to View</p>
